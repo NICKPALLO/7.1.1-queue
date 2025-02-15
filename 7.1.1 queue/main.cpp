db8581406b4daff5a506dbec81deace9333013be
Nikolay <yakolya19@gmail.com>
2025-02-15 11:30:01 +0400

    7.2.1 atomic queue

--- 7.1.1 queue/main.cpp
+++ 7.1.1 queue/main.cpp
@@ -1,28 +1,29 @@
 #include<iostream>
 #include<thread>
 #include<chrono>
+#include<atomic>
 #include<Windows.h>
 
 
 using namespace std::chrono_literals;
 
-void add_client(int* queue, int* total_clients)
+void add_client(std::atomic<int>& queue, std::atomic<int>& total_clients)
 {
-	while (*total_clients < 25)
+	while (total_clients < 15)
 	{
-		++(*queue);
-		++(*total_clients);
+		++queue;
+		++total_clients;
 		std::this_thread::sleep_for(500ms);
 	}
 }
 
-void remove_client(int* queue)
+void remove_client(std::atomic<int>& queue, std::atomic<int>& total_clients)
 {
-	while (*queue > 0)
+	while (queue > 0)
 	{
 		system("cls");
-		--(*queue);
-		std::cout << "Человек в очереди: " << *queue;
+		--queue;
+		std::cout << "Человек в очереди: " << queue;
 		std::this_thread::sleep_for(1s);
 	}
 }
@@ -33,13 +34,15 @@ void remove_client(int* queue)
 
 int main()
 {
+
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	int queue = 5;
-	int total_clients = 5;
-	std::thread th1(add_client, &queue, &total_clients);
-	std::thread th2(remove_client, &queue);
+	std::atomic<int> queue = 5;
+	std::atomic<int> total_clients = 5;
+
+	std::thread th1(add_client,std::ref(queue),std::ref(total_clients));
+	std::thread th2(remove_client, std::ref(queue), std::ref(total_clients));
 
 	th1.join();
 	th2.join();
